0c1cacf692261c353ddf0d90983be6b88ee3e45e
Nicolas Wailly <nicolas.wailly.dev@outlook.com>
2021-05-30 12:34:58 +0200

    Fix error exercice 104

--- basic/exercice-104.js
+++ basic/exercice-104.js
@@ -6,6 +6,7 @@ var numbers = [
     {serie: [12, 10, 33, 34], target: 10},
     {serie: [12, 10, 33, 34], target: 24},
     {serie: [12, 10, 33, 44], target: 40},
+    {serie: [33, 10, 33, 44], target: 10}
 ]; 
 var output = ``;
 
@@ -15,15 +16,14 @@ numbers.forEach(item => {
         secondNumber: 0,
         absDiff: 0};
     
-    for (let i = 0; i < item.serie.length; i++) {
-        for (let j = i + 1; j < item.serie.length -1; j++) {
-            let diff = Math.abs(item.serie[i] - item.serie[j]);
-            if(diff <= item.target){
-                if(number.absDiff < diff){
-                number.absDiff = diff;
-                number.firstNumber = item.serie[i];
-                number.secondNumber = item.serie[j];}
-            }
+    for (let i = 0; i < item.serie.length - 1; i++) {
+        for (let j = i + 1; j < item.serie.length; j++) {
+            let diff = Math.abs(item.serie[i] - item.serie[j]);            
+                if(Math.abs(item.target - diff) < item.target - number.absDiff){
+                    number.firstNumber = item.serie[i];
+                    number.secondNumber = item.serie[j];
+                    number.absDiff = diff;
+                }            
         }  
     }
     output += `[${item.serie}] - [${item.target}] -> [${number.firstNumber},${number.secondNumber}] - [${number.absDiff}]\n`;
